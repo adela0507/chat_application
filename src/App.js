7770718ce61f766f3c424514d724c9ea28f72ece
adela miclea <adelamiclea709@yahoo.com>
2025-11-16 19:29:44 +0200

    sendbird settings

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
+import '@sendbird/uikit-react/dist/index.css';
+import { App as SendbirdApp } from '@sendbird/uikit-react';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        </p>
-        
-      </header>
+    <div style={{ width:'100vw', height:'100vh' }}>
+      {}
+      <SendbirdApp
+        appId={'C748068E-29BD-462A-A3D5-5E65FBBC12C3'}
+        userId={'sendbird_desk_agent_id_50ef85a7-a1b7-4d38-8957-c1afd33b3442'}
+        accessToken={'ae6011f8df9988771757859f80ba9d9bda45f3fb'} 
+      />
     </div>
   );
 }
